24ac3b55bb747c45fa5a0072fb8eaea600a231ae
ohmrr <89240639+ohmrr@users.noreply.github.com>
2026-02-24 23:13:58 -0800

    - Create form700_parser.py, with method load_form700.csv - Method takes in a file_path for now, which is the given excel file converted to a csv and extracts each employee's name, the agency they work for, their position, and what holdings they have - set up some demo endpoints for thursdays presentation

--- server/app/main.py
+++ server/app/main.py
@@ -1,11 +1,33 @@
 from fastapi import FastAPI
+from server.app.services.legistar_client import LegistarClient
+from server.app.services.form700_parser import load_form700_csv
 
 app = FastAPI(
     title="FPPC Conflict of Interest Identifier",
-    description="Backend API for checking potential conflicts of interest for public officials",
-    version="0.0.1"
+    version="0.0.1",
 )
+
+# After getting the server running, open this url in your browser
+# This will open up Swagger, which can be used to showcase the API
+print("API Documentation - http://127.0.0.1:8000/docs")
+
 
 @app.get("/")
 def root():
-    return { "message": "App is running successfully!" }
+    return {"message": "FPPC API is running successfully!"}
+
+
+# Below are the demo endpoints
+
+
+# Use "sonoma-county" for client_name
+@app.get("/persons/{client_name}")
+def get_persons(client_name: str):
+    return LegistarClient(client_name).get_persons()
+
+
+# Use "sonoma-county" for client name
+# Use 2021 for year
+@app.get("/officials/{client_name}/{year}")
+def get_officials(client_name: str, year: int):
+    return load_form700_csv(f"./server/app/data/{client_name}-{year}.csv")
