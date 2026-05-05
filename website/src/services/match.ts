a446e971831cd0e6e2ce86fc3019abe0aa776ea9
Shiven Singh <149639333+ShivenSingh1@users.noreply.github.com>
2026-05-05 13:16:56 -0700

    add delete matches feature

--- website/src/services/match.ts
+++ website/src/services/match.ts
@@ -5,3 +5,9 @@ export const getMatches = () => apiFetch<Match[]>(`/matches`)
 
 export const getMatchesByJurisdiction = (jurisdiction: string) =>
   apiFetch<Match[]>(`/matches/${jurisdiction}`)
+
+//added
+export const deleteMatch = (id: number) =>
+  apiFetch(`/matches/${id}`, {
+    method: "DELETE",
+  })
